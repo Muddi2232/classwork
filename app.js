b09c7c190a53bf0351799a6c1cc6ffd0cd4d53bb
Muddi2232 <154959246+Muddi2232@users.noreply.github.com>
2024-05-19 15:28:59 +0500

    sunday

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 
-var para= document.getElementById('paragraph');
+var boldText= document.getElementById('bold'),
+italicText = document.getElementById('italic'),
+underlineText =document.getElementById('underline'),
+para =document.getElementById ('paragraph');
+
+boldText.addEventListener('click', function(){
+    para.style.fontWeight= 'bold';
+});
+italicText.addEventListener('click', function(){
+    para.style.fontStyle= 'italic';
+});
+underlineText.addEventListener('click', function(){
+    para.style.textDecoration= 'underline';
+});
